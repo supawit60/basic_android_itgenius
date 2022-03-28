612dc29b820033ff69dfbb08165c9011db947ab9
ssupawit.k <supawit@odds.team>
2022-03-28 23:36:54 +0700

    02 variables

---
+++ src/main/kotlin/02_Variables.kt
@@ -0,0 +1,11 @@
+fun kVariables() {
+    val pi: Float = 3.141f
+    val pricePerUnit: Short = 1000
+    val lat: Double = 100.12345
+    val lon: Double = 80.67890
+
+    var kByte: Byte = 100
+    println(kByte)
+    kByte = 127
+    println(kByte)
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
 fun main() {
-    kDataType()
+//    kDataType()
+    kVariables()
 }

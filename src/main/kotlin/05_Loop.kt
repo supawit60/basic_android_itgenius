42fd3548757a0abcb4225337c7a0247800da6d66
ssupawit.k <supawit@odds.team>
2022-03-29 21:21:41 +0700

    05 Loop for-in

--- src/main/kotlin/05_Loop.kt
+++ src/main/kotlin/05_Loop.kt
@@ -4,4 +4,15 @@ fun kLoop() {
         println(x)
     }
 
+    println("-----------------")
+    val min = 1
+    val max = 100
+    var count = 0
+    for (i in min..max) {
+        if (i % 3 == 0) {
+            count++
+        }
+    }
+    println("จาก $min - $max มีเลขที่ 3 หารลงตัวเท่ากับ $count จำนวน")
+
 }

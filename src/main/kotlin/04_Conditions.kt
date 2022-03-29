783df043b34c95914c9d2e68dd6c897575bcbe6c
ssupawit.k <supawit@odds.team>
2022-03-29 21:04:57 +0700

    04 Conditional Expression

--- src/main/kotlin/04_Conditions.kt
+++ src/main/kotlin/04_Conditions.kt
@@ -2,14 +2,24 @@ fun kConditions() {
     var balance = 30_000
     val withdraw = 2_510
     if (balance < withdraw) {
-        print("ยอดเงินคงเหลือไม่เพียงพอ")
+        print("ยอดเงินคงเหลือไม่เพียงพอ\n")
     } else if (withdraw > 20_000) {
-        print("ถอนได้ไม่เกิน 20000 บาท")
+        print("ถอนได้ไม่เกิน 20000 บาท\n")
     } else if (withdraw % 100 != 0) {
-        print("ต้องเป็นจำนวนเต็มร้อยเท่านั้น")
+        print("ต้องเป็นจำนวนเต็มร้อยเท่านั้น\n")
     } else {
         balance -= withdraw
         print("ยอดเงินคงเหลือ: $balance บาท\n")
     }
+
+    println("-----------------")
+    val remain = 1
+    val r = if (remain > 0) "มีสินค้า" else "สินค้าหมด"
+    println(r)
+    println("-----------------")
+    val ext = ".kt"
+    println(if (ext == ".java") "Java File" else "Kotlin File")
+    println("-----------------")
+
 
 }
